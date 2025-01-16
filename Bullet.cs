7173bdf5cf166d74779fda192aa2937447010e42
Roman Strakhov <43264457+Rubik48@users.noreply.github.com>
2025-01-16 18:01:15 -0500

    4

--- Bullet.cs
+++ Bullet.cs
@@ -1,3 +1,15 @@
 using UnityEngine;
 
-public class Bullet : MonoBehaviour {}
+[RequireComponent(typeof(Rigidbody))]
+public class Bullet : MonoBehaviour 
+{
+    private Rigidbody _rigidbody;
+    
+    private void Awake() => _rigidbody = GetComponent<Rigidbody>();
+    
+    public void SetMoveDirection(Vector3 direction, float _bulletSpeed)
+    {
+     _rigidbody.transform.up = direction;
+     _rigidbody.linearVelocity = direction * _bulletSpeed;
+    }
+}
